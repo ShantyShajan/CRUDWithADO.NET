bf45c7e345f6ec0cf5a1799f33a5f5411a5769bc
Shanty Shajan <shanty141199@gmail.com>
2024-06-16 20:21:28 +0100

    first commit

---
+++ CRUDWithADO.NET/DAL/IEmployee_DAL.cs
@@ -0,0 +1,14 @@
+﻿using CRUDWithADONET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDWithADONET.DAL
+{
+    public interface IEmployee_DAL
+    {
+        public List<Employee> GetAll();
+        public bool Insert(Employee model);
+        public Employee GetById(int id);
+        public bool Update(Employee model);
+        public bool Delete(int Id);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# CRUDWithADO.NET" 
